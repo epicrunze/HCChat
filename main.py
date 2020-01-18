8040f195de0996a7e38d26835ee0a5e8abf2f588
Fungucide <bluestich227@gmail.com>
2020-01-18 09:16:00 -0500

    Auto-link webhook

--- main.py
+++ main.py
@@ -1,17 +1,27 @@
 import webhook
+import authentication
 from flask import Flask, render_template, request
 
+# Important values
+WEBHOOK_URL="https://praxis-atrium-265504.appspot.com/webhook"
+AUTH_KEY=""
+AUTH_EXPIRY=""
+
+# Init Flask
 app = Flask(__name__)
-
-
-WEBHOOK_URL="https://praxis-atrium-265504.appspot.com/test-post"
+# Get the AUTH_KEY for the chat bot
+AUTH_KEY,AUTH_EXIPRY=authentication.getAuthKey('chat','chat@bot','uofthacksteam2','Lu7qXWP3b3d3')
+# Unlink any previous webhooks in place
+webhook.remove()
+# Re-link the web app 
+webhook.addWebhook(WEBHOOK_URL, AUTH_KEY)
 
 @app.route('/')# Root index for later :)
 def root():
     return render_template('index.html')
     
 
-@app.route('/test-post', methods=['POST']) #allow only POST requests
+@app.route('/webhook', methods=['POST']) # Allow only POST requests
 def messageReceived():
     if request.method != 'POST':# Something went terribly wrong
         raise RuntimeError('A non-POST request was revieved by this function')
