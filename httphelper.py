ed0c12d8b1bb04f141a31a760bf1cb44d017657e
Fungucide <bluestich227@gmail.com>
2020-01-18 10:29:25 -0500

    Fixed sending messages

---
+++ httphelper.py
@@ -0,0 +1,9 @@
+import requests
+import base64
+
+def post(url:str,headers:dict,payload:str):
+    return requests.request("POST", url, headers=headers, data = payload)
+
+def encodeBase64(s:str)->str:
+    encodedBytes = base64.b64encode(s.encode("utf-8"))
+    return str(encodedBytes, "utf-8")
