cf358fe681aa5811d28cc5b29fec0724a9c1b828
Fungucide <bluestich227@gmail.com>
2020-01-18 01:35:12 -0500

    Webhooks complete

---
+++ main.py
@@ -0,0 +1,5 @@
+import webhook
+WEBHOOK_URL="https://praxis-atrium-265504.appspot.com/"
+AUTH="cd5a0a3d9909103835b100851a2b6fe528b8114b"
+print(webhook.addWebhook(WEBHOOK_URL,AUTH))
+print(webhook.removeWebhook(AUTH))
