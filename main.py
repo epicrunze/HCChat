63f881408f389c3d764ff34c5235f0e20637bb49
George Saad <g.saad@mail.utoronto.ca>
2020-01-18 03:00:45 -0500

    Update main.py

--- main.py
+++ main.py
@@ -3,4 +3,5 @@ REQUEST_BIN="https://praxis-atrium-265504.appspot.com/test-post"
 WEBHOOK_URL="https://enjjibedx31jf.x.pipedream.net"
 AUTH="cd5a0a3d9909103835b100851a2b6fe528b8114b"
 print(webhook.addWebhook(WEBHOOK_URL,AUTH))
+print(webhook.addWebhook(REQUEST_BIN,AUTH))
 #print(webhook.removeWebhook(AUTH))
