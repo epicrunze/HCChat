6d5c12b88cb238ba01a2c3d3487d45b38e78315f
George Saad <g.saad@mail.utoronto.ca>
2020-01-18 01:54:32 -0500

    more testing, works

--- main.py
+++ main.py
@@ -9,12 +9,10 @@ app = Flask(__name__)
 def root():
     # For the sake of example, use static information to inflate the template.
     # This will be replaced with real information in later steps.
-    dummy_times = [datetime.datetime(2018, 1, 1, 10, 0, 0),
-                   datetime.datetime(2018, 1, 2, 10, 30, 0),
-                   datetime.datetime(2018, 1, 3, 11, 0, 0),
-                   ]
+    dummy_times = ["zoo","roo","moo"]
+                   
 
-    return render_template('index.html', times=dummy_times)
+    return render_template('index.html', time="yee")
 
 
 if __name__ == '__main__':
