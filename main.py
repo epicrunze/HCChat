e2aaa732d3ec42684d37f6e970f7adeaeace5270
Fungucide <bluestich227@gmail.com>
2020-01-18 07:36:09 -0500

    Fixed up main
    Removed all non-essential parts to main

--- main.py
+++ main.py
@@ -1,40 +1,21 @@
-import httphelper
-
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/')# Root index for later :)
 def root():
-    # For the sake of example, use static information to inflate the template.
-    # This will be replaced with real information in later steps.
-    dummy_times = ["zoo","roo","moo"]
-                   
-
-    return render_template('index.html', time="yee")
+    return render_template('index.html')
     
 
-@app.route('/test-post', methods=['GET', 'POST']) #allow both GET and POST requests
-def form_example():
-    if request.method == 'POST': #this block is only entered when the form is submitted
-        httphelper.post("https://enjjibedx31jf.x.pipedream.net/",{},"post")
-        print("zoo")
-        return
-    httphelper.post("https://enjjibedx31jf.x.pipedream.net/",{},"form")
-    return '''<form method="POST">
-                  Language: <input type="text" name="language"><br>
-                  Framework: <input type="text" name="framework"><br>
-                  <input type="submit" value="Submit"><br>
-              </form>'''
+@app.route('/test-post', methods=['POST']) #allow only POST requests
+def messageReceived():
+    if request.method != 'POST':# Something went terribly wrong
+        raise RuntimeError('A non-POST request was revieved by this function')
+    #Put code here to handle messages
+    
 
 
 if __name__ == '__main__':
-    # This is used when running locally only. When deploying to Google App
-    # Engine, a webserver process such as Gunicorn will serve the app. This
-    # can be configured by adding an `entrypoint` to app.yaml.
-    # Flask's development server will automatically serve static files in
-    # the "static" directory. See:
-    # http://flask.pocoo.org/docs/1.0/quickstart/#static-files. Once deployed,
-    # App Engine itself will serve those files as configured in app.yaml.
+    #Debuging locally
     app.run(host='127.0.0.1', port=8080, debug=True)
