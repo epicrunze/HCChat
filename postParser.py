219c493cb666a012a4598f9d87c36ba6a97339c2
Fungucide <bluestich227@gmail.com>
2020-01-18 19:58:01 -0500

    Fixed non-existant issues

--- postParser.py
+++ postParser.py
@@ -2,6 +2,8 @@ import json
 import chatbot
 import message
 
+f = open('chats.dat','a')
+
 def parsePost(s:str,chatbots:dict,cred:tuple, orgId:int, botId:str):
     jObject=json.loads(s)
     chatId=jObject['message']['chatId']
@@ -10,5 +12,7 @@ def parsePost(s:str,chatbots:dict,cred:tuple, orgId:int, botId:str):
         return
     if(chatId not in chatbots):
         chatbots[chatId]=chatbot.Chatbot(*cred)
+        f.write(chatId+'\n')
+        f.flush()
     response = chatbots[chatId].parseString(msg)
     message.sendMessage(chatbots[chatId].accessToken,chatId,orgId,response)
