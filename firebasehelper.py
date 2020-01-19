5ca1954d8b7e9303225cba964f46fa383e02ee14
Ryan Zhang <runze.zhang2001@gmail.com>
2020-01-19 01:36:56 -0500

    changed to be from firebase

--- firebasehelper.py
+++ firebasehelper.py
@@ -1,13 +1,23 @@
 from datetime import date
 import httphelper
+import json
 
 URL='https://hyperbase-f0472.firebaseio.com/'
 
-def writeData(data:dict,ext='':str)->None:
-    data=str(data)
+def writeData(data:dict,ext:str='chatbot/'):
+    data=str(data).replace("'", '\"')
     header={'content-type':'application/json'}
     return httphelper.patch(URL+ext+'.json',header,data)
 
-def getData(chatId:str,ext='':str)->str:
+def getData(chatId:str,ext:str='chatbot/')->str:
     header={'content-type':'application/json'}
     return httphelper.get(URL+ext+chatId+'.json',header).text
+
+def json2Dict(string):
+    return json.loads(string)
+
+def getDict(chatId, ext='chatbot/'):
+    return json2Dict(getData(chatId, ext))
+
+def writeDict(data, chatId, ext='chatbot/'):
+    return writeData({str(chatId): data})
