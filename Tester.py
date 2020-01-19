bacb97d43d703056864a39e335842543e499e48d
Fungucide <bluestich227@gmail.com>
2020-01-19 02:31:13 -0500

    Cleaned up files

--- Tester.py
+++
@@ -1,7 +0,0 @@
-import authentication
-username = "ryanzhang"
-password = "pass@word"
-clientId = "uofthacksteam2"
-clientSecret = "Lu7qXWP3b3d3"
-
-print(authentication.getId(authentication.getAuthKey(username, password, clientId, clientSecret)[0], orgId=71))
