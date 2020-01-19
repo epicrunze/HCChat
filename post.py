10f22e76882057e4c63169ed5cdd523ed90b1474
Fungucide <bluestich227@gmail.com>
2020-01-19 02:05:42 -0500

    rename

--- post.py
+++ post.py
@@ -4,15 +4,14 @@ import message
 import firebasehelper
 import httphelper
 
-def parsePost(s:str,chatbots:dict,cred:tuple, orgId:int, botId:str)->None:
+def parsePost(s:str,chatbots:dict, cred:tuple, orgId:int, botId:str)->None:
     jObject=json.loads(s)
     chatId=jObject['message']['chatId']
     msg=jObject['message']['message']
     if(jObject['message']['userId']==botId):
         return
     if(firebasehelper.getData(chatId)=='null'):
-        cred = tuple(chatId) + cred + tuple(jObject['message']['userId'])
-        res=chatbot.initialize(*cred)
+        res=chatbot.initialize(chatId)
     response = chatbots.parseString(chatId,msg)
     accessToken = firebasehelper.getDict(chatId)['accessToken']
     message.sendMessage(accessToken,chatId,orgId,response)
