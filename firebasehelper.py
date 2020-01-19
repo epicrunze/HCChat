7d1223cc4d435c8a11dad0c747203988adb25947
Fungucide <bluestich227@gmail.com>
2020-01-19 00:15:58 -0500

    Update firebasehelper.py

--- firebasehelper.py
+++ firebasehelper.py
@@ -3,11 +3,11 @@ import httphelper
 
 URL='https://hyperbase-f0472.firebaseio.com/users/'
 
-def writeData(chatId:str)->None:
-    data='{"'+chatId+'":"'+str(date.today())+'"}'
+def writeData(data:dict)->None:
+    data=str(data)
     header={'content-type':'application/json'}
     return httphelper.patch(URL+'.json',header,data)
 
-def hasChatId(chatId:str)->bool:
+def getData(chatId:str)->str:
     header={'content-type':'application/json'}
-    return httphelper.get(URL+chatId+'.json',header).text!='null'
+    return httphelper.get(URL+chatId+'.json',header).text
