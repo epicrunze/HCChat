ce654ef0b9dae6b3f347b8a171aead15b1fc4044
Fungucide <bluestich227@gmail.com>
2020-01-18 22:01:18 -0500

    Added firebase cappabilities

--- httphelper.py
+++ httphelper.py
@@ -4,6 +4,9 @@ import base64
 def post(url:str,headers:dict,payload:str):
     return requests.request("POST", url, headers=headers, data = payload)
 
+def put(url:str,headers:dict,payload:str):
+    return requests.request("PUT", url, headers=headers, data=payload)
+
 def encodeBase64(s:str)->str:
     encodedBytes = base64.b64encode(s.encode("utf-8"))
     return str(encodedBytes, "utf-8")
--- postParser.py
+++ postParser.py
@@ -1,18 +1,20 @@
 import json
 import chatbot
 import message
+import firebasehelper
+import httphelper
 
-f = open('chats.dat','a')
-
-def parsePost(s:str,chatbots:dict,cred:tuple, orgId:int, botId:str):
+def parsePost(s:str,chatbots:dict,cred:tuple, orgId:int, botId:str)->None:
     jObject=json.loads(s)
     chatId=jObject['message']['chatId']
     msg=jObject['message']['message']
     if(jObject['message']['userId']==botId):
         return
     if(chatId not in chatbots):
-        chatbots[chatId]=chatbot.Chatbot(*cred)
-        f.write(chatId+'\n')
-        f.flush()
+        if(firebasehelper.hasChatId(chatId)):
+            httphelper.post('localhost/webhook',{},s)
+        else:
+            chatbots[chatId]=chatbot.Chatbot(*cred)
+            firebaseHelper.writeData(chatId)
     response = chatbots[chatId].parseString(msg)
     message.sendMessage(chatbots[chatId].accessToken,chatId,orgId,response)
