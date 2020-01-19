6ea76b181f484521cc9b89077f847707dc8821ee
Fungucide <bluestich227@gmail.com>
2020-01-19 01:26:01 -0500

    Update API connections

--- firebasehelper.py
+++ firebasehelper.py
@@ -3,11 +3,11 @@ import httphelper
 
 URL='https://hyperbase-f0472.firebaseio.com/'
 
-def writeData(data:dict,ext='':str)->None:
+def writeData(data:dict,ext:str='')->None:
     data=str(data)
     header={'content-type':'application/json'}
     return httphelper.patch(URL+ext+'.json',header,data)
 
-def getData(chatId:str,ext='':str)->str:
+def getData(chatId:str,ext:str='')->str:
     header={'content-type':'application/json'}
     return httphelper.get(URL+ext+chatId+'.json',header).text
