4850b1f7885082044802d6ef6a7722f9e53ddaa4
Fungucide <bluestich227@gmail.com>
2020-01-19 00:22:57 -0500

    Update firebasehelper.py

--- firebasehelper.py
+++ firebasehelper.py
@@ -1,13 +1,13 @@
 from datetime import date
 import httphelper
 
-URL='https://hyperbase-f0472.firebaseio.com/users/'
+URL='https://hyperbase-f0472.firebaseio.com/'
 
-def writeData(data:dict)->None:
+def writeData(data:dict,ext='':str)->None:
     data=str(data)
     header={'content-type':'application/json'}
-    return httphelper.patch(URL+'.json',header,data)
+    return httphelper.patch(URL+ext+'.json',header,data)
 
-def getData(chatId:str)->str:
+def getData(chatId:str,ext='':str)->str:
     header={'content-type':'application/json'}
-    return httphelper.get(URL+chatId+'.json',header).text
+    return httphelper.get(URL+ext+chatId+'.json',header).text
